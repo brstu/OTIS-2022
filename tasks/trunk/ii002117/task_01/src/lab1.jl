9bf49f25d3df76993570df415fa63aae5ae75c3e
Shpak Ilya <113047080+IlyaShpak@users.noreply.github.com>
2022-10-02 20:08:26 +0300

    Update lab1.jl

--- tasks/trunk/ii002117/task_01/src/lab1.jl
+++ tasks/trunk/ii002117/task_01/src/lab1.jl
@@ -1,11 +1,11 @@
-using Plots
+using PyPlot
 function lineral_model(time,weigth)
     warm=1
     y=1
     result=[]
     for i in 1:time
-        y=weigth[1]*y+weigth[2]*warm
-        push!(result,y)
+	y=weigth[1]*y+weigth[2]*warm
+	push!(result,y)
     end
     return result
 end
@@ -15,21 +15,28 @@ function nonlineral_model(time,weigth)
     start_warm=1
     result=[]
     for i in 1:time
-        future_y=weigth[1]*current_y-weigth[2]*previous_y^2+weigth[3]*start_warm+weigth[4]*sin(start_warm)
-        previous_y=current_y
-        current_y=future_y
-        push!(result,current_y)
+	future_y=weigth[1]*current_y-weigth[2]*previous_y^2+weigth[3]*start_warm+weigth[4]*sin(start_warm)
+	previous_y=current_y
+	current_y=future_y
+	push!(result,current_y)
     end
     return result
 end
 function main()
     weigth_for_lineral=[1,2]
-    weigth_for_nonlineral=[0.1,0.8,0.9,1.9]
-    x_line=[i for i in 1:8]
-    lineral_result=lineral_model(8,weigth_for_lineral)
-    nonlinreal_result=nonlineral_model(8,weigth_for_nonlineral)
-    println(nonlinreal_result)
-    a=plot(x_line,lineral_result,color="red",label="lineral")
-    plot!(x_line,nonlinreal_result,color="blue",label="non_lineral")
+    weigth_for_nonlineral=[0.8,0.09,1,10.1]
+    x_line=[i for i in 1:50]
+    lineral_result=lineral_model(50,weigth_for_lineral)
+    nonlinreal_result=nonlineral_model(50,weigth_for_nonlineral)
+    println("Линейная модель ")
+    for i in lineral_result
+	println(i)
+    end
+    println("Нелинейная модель ")
+    for i in nonlinreal_result
+	println(i)
+    end
+    plot(x_line,lineral_result,color="red",label="lineral")
+    plot(x_line,nonlinreal_result,color="blue",label="non_lineral")
 end
 main()
