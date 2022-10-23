b8b6a64d79d8aed611b1dd51a804851781e5d81f
Corowka <107122474+Corowka@users.noreply.github.com>
2022-10-24 00:56:02 +0300

    code done

--- trunk/ii2209/task_01/src/task_01.jl
+++ trunk/ii2209/task_01/src/task_01.jl
@@ -1,56 +1,53 @@
-myRound(value, amount = 3) = floor(value * 10 ^ amount) / 10 ^ amount
+using Plots
 
-const time_speed = 1 #time speed in seconds
+#models
 
-const a = 0.523
-const b = 0.253
-const c = 2.234
-const d = 0.923
-
-u = 1 #warm
-
-println("Input values:")
-println("a = $(a)")
-println("b = $(b)")
-println("c = $(c)")
-println("d = $(d)")
-println("warm = $(u)")
-
-y = 1 #tempreture for linear model
-
-function linear_tInc_modele(time)
-    println("\nLinear model has been started: tempreture is rising (start value = $(y)).")
-    for i = 1:time 
-        prev_y = y
-        global y = a * y + b * u
-        println((prev_y < y) ? "Tempreture is rising (now: $(myRound(y))). $(time - i) seconds left." : "Tempreture is dropping (now: $(myRound(y))). $(time - i) seconds left.")
-        sleep(time_speed)
-    end 
-end
-
-linear_tInc_modele(10)
-
-y = 1 #tempreture for unlinear model
-prev_y = 0 #previous tempreture (1 second ago)
-
-function unlinear_tInc_modele(time) 
-    println("\nUnlinear model has been started: tempreture is rising (start values[pver_y, y] = $(prev_y), $(y)).")
-    for i = 1:time
-        new_y = a * y - b * prev_y ^ 2 + c * u + d * sin(u)
-        global prev_y = y
-        global y = new_y
-        println((prev_y < y) ? "Tempreture is rising (now: $(myRound(y))). $(time - i) seconds left." : "Tempreture is dropping (now: $(myRound(y))). $(time - i) seconds left.")
-        sleep(time_speed)
+function linear_heat(time)
+    global y = []
+    push!(y, 0.0)
+    println("Linear heat:")
+    for i in 1:time
+        push!(y, round(a * y[i] + b * u, digits = 1))
+        println("t = ", y[i], " current time is ", i, 's')
     end
 end
 
-unlinear_tInc_modele(10)
-
-println("\nThe program is over!")
-
-end_time = 30
-println("Window will be closed for $(end_time) seconds.")
-for i = 1:end_time
-    print("[]")
-    sleep(1)
+function nonlinear_heat(time)
+    global y = []
+    push!(y, 0.0)
+    push!(y, 1.0)
+    println("nonLinear heat:")
+    for i in 2:time
+        push!(y, round(a * y[i] - b * y[i - 1] ^ 2 + c * u + d * sin(u), digits = 1))
+        println("t = ", y[i], " current time is ", i, 's')
+    end
 end
+
+#settings
+
+a = 0.923
+b = 0.052
+c = 0.433
+d = 0.254
+u = 20
+y = []
+
+time = 10
+
+
+#main
+
+function main() 
+    x = 1:time
+    linear_heat(time)
+    y = 1:10
+    plot(x, y)
+    nonlinear_heat(time)
+    y2 = [ 1, 3, 10]
+    plot(x, y)
+    plot(x, yz)
+end
+
+main()
+
+
