ee804c2fff1e28fa2b5c23163b8618f8c877bdfe
Boreysha Oleg <boreysha.oleg@gmail.com>
2022-11-09 05:09:23 +0300

    Update lab1.jl

--- trunk/ii02201/task_01/src/lab1.jl
+++ trunk/ii02201/task_01/src/lab1.jl
@@ -1,29 +1,58 @@
-function pow(value,degree)
-    result= 1
-    for i in 1:degree
-        result= result * value
+using Plots
+
+function Linear_model(y_curr, u, t)
+    a = 0.8
+    b = 0.3
+    Arr = []
+    push!(Arr,y_curr)
+    for i in 1:t
+        y_next = a * y_curr + b * u
+        push!(Arr,y_next)
+        y_curr = y_next
     end
-    return result
+    println(Arr)
+    plot([1:(t+1)], Arr, color = :orange , label = "Linear_model")
 end
 
-function Linear_Model(a,b,y_current,u_current)
-    println(a*y_current+b*u_current)
-    return (a*y_current+b*u_current)
+function Unlinear_model(y_curr, u_curr, t)
+    a = 1
+    b = 1
+    c = 1
+    d = 1
+    y_prev = 0
+    u_prev = 0
+    Arr = []
+    push!(Arr,y_curr)
+    for i in 1:t
+        y_next= a * y_curr - b * y_prev^2 + c * u_curr + d * sin(u_prev)
+        push!(Arr,y_next)
+        y_prev = y_curr
+        y_curr = y_next
+        u_prev = u_curr
+        u_curr = u_curr - 1
+    end
+    println(Arr)
+    plot([1:(t+1)], Arr, color = :blue , label = "Unlinear_model")
 end
 
-function UnLinear_Model(a, b, c, d, y_current, y_prev, u_current, u_prev)
-    y_new = a*y_current - b * pow(y_prev,2) + c*u_current + d*sin(u_prev)
-    println(y_new)
-    return y_new
+function getInt()
+    x = chomp(readline())
+    try
+        return parse(Int,x)
+    catch
+        return Nothing
+    end
 end
 
 function main()
-    a= 
-    b=
-    c=
-    d=
-    y=0.0
-    u=0.0
+    println("Enter ambient temperature: ")
+    start_temp = getInt()                    # the initial temperature value is equal to the ambient temperature
+    println("Enter warm input value: ")
+    warm = getInt()                          # warm input value of the corresponding simulation object
+    println("Enter process duration: ")
+    duration = getInt()
+    Linear_model(start_temp, warm, duration)
+    Unlinear_model(start_temp, warm, duration)
 end
 
 main()
