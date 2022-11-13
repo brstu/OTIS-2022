b54a22c1743828bacd0e912d15c069688eb09329
COROWKA <kopanchuke@gmail.com>
2022-11-13 03:01:58 +0300

    Update task_01.jl

--- trunk/ii02209/task_01/src/task_01.jl
+++ trunk/ii02209/task_01/src/task_01.jl
@@ -6,7 +6,7 @@ a_value = 0.923
 b_value = 0.052
 c_value = 0.433
 d_value = 0.254
-room_temprature = 12
+room_warm = 12
 heat_array = []
 
 time = 10
@@ -18,14 +18,14 @@ time_points = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 heat_array = [0.0]
     println("Linear heat:")
     for iterator in 1:time - 1
-        push!(heat_array, round(a_value * y[iterator] + b_value * room_temprature, digits = 1))
+        push!(heat_array, round(a_value * y[iterator] + b_value * room_warm, digits = 1))
         println("t = ", heat_array[iterator], " current time is ", iterator, 's')
     end
 plot(time_points, heat_array, color="green", labal="linear")
 heat_array = [0.0, 1.0]
     println("nonLinear heat:")
     for iterator in 2:time - 1
-        push!(heat_array, round(a_value * heat_array[iterator] - b_value * heat_array[iterator - 1] ^ 2 + c_value * room_temprature + d_value * sin(room_temprature), digits = 1))
+        push!(heat_array, round(a_value * heat_array[iterator] - b_value * heat_array[iterator - 1] ^ 2 + c_value * room_warm + d_value * sin(room_warm), digits = 1))
         println("t = ", heat_array[iterator], " current time is ", iterator, 's')
     end
 plot!(time_points, heat_array, color="blue", labal="not linear")
