0f1d31315109ec6646df02cca3d006459cf87010
Nika Levanevskaya <chipolinchikaloe@gmail.com>
2022-11-15 01:01:20 +0300

    lllaaaabaaa

--- trunk/ii02212/task_01/src/lab1.jl
+++ trunk/ii02212/task_01/src/lab1.jl
@@ -3,10 +3,10 @@ using Plots
 
 value_u_t = 0.5 # input warm
 _this_is_a_value_time_ = 15 # time
-this_is_a_constant_a = 1.2  # constant
-this_is_a_constant_b = 0.7  # constant
-this_is_a_constant_c = -0.32  # constant
-this_is_a_constant_d = 0.53 # constant
+this_is_a_constant_whanoke_a = 1.2  # constant
+this_is_a_constant_karekau_b = 0.7  # constant
+this_is_a_constant_uwha_c = -0.32  # constant
+this_is_a_constant_tonu_d = 0.53 # constant
 
 array_yt_linear = []  #array for linear dependency
 array_yt_not_linear = [] #array for non-linear dependency
@@ -16,7 +16,7 @@ function my_first_function_()
     println("linear model")
     _y_t_ = 1 # input temperature
     for i in 1 : _this_is_a_value_time_
-        _y_t_ = this_is_a_constant_a * yt + this_is_a_constant_b * value_u_t
+        _y_t_ = this_is_a_constant_whanoke_a * yt + this_is_a_constant_karekau_b * value_u_t
         println(_y_t_)
         push!(array_yt_linear, _y_t_)
     end
@@ -29,7 +29,7 @@ function my__second__function_()
     println("non-linear model")
     for i in 1 : _this_is_a_value_time_
         temperature = yt_previous
-        _y_t_ = this_is_a_constant_a * _y_t_ - this_is_a_constant_b * yt_previous ^ 2 + this_is_a_constant_c * value_u_t + this_is_a_constant_d * sin(value_u_t)
+        _y_t_ = this_is_a_constant_whanoke_a * _y_t_ - this_is_a_constant_karekau_b * yt_previous ^ 2 + this_is_a_constant_uwha_c * value_u_t + this_is_a_constant_tonu_d * sin(value_u_t)
         yt_previous = temperature
         println(_y_t_)
         push!(array_yt_not_linear, _y_t_)
