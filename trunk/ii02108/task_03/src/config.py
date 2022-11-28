d88737daf9c7a8aed0f06a5ae6c3fdd23a96451b
Литвинюк Тимофей <108889278+4eburaWka@users.noreply.github.com>
2022-11-28 22:49:07 +0300

    Update config.py

--- trunk/ii02108/task_03/src/config.py
+++ trunk/ii02108/task_03/src/config.py
@@ -2,3 +2,8 @@ main_theme = 'dark'
 btns_color = 'black'
 add_tab_button = 'green'
 close_tab_button = 'red'
+default_btn_clr = 'white'
+selected_tab_clr = '#388fd1'
+
+path_to_img_create_new_tab = 'E:\Studing\OTIS\lab3\icons\icon_for_newgraph.png'#'./icons/icon_for_newgraph.png'
+path_to_img_close_tab = 'E:\Studing\OTIS\lab3\icons\icon_for_close_tab.png'#'./icons/icon_for_close.png'
