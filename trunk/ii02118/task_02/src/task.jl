ddb947c6a822c33dd213f45e4b816a95051d3adb
Shurina Angelina <120337170+pashtettttt@users.noreply.github.com>
2022-12-14 21:55:19 +0300

    Update task.jl

--- trunk/ii02118/task_02/src/task.jl
+++ trunk/ii02118/task_02/src/task.jl
@@ -1,23 +1,23 @@
-J = 0.4; G = 0.7; V = 0.8 #укажем здесь наши параметры
- B = 0.5; L = 0.7; T9 = 1.1
- HP = 1; H = 1.1; SS = 2.0; Q = 20
+а = 0.4; б = 0.7; в = 0.8 #укажем здесь наши параметры
+ г = 0.5; л = 0.7; т9 = 1.1
+ хр = 1; х = 1.1; ус = 2.0; к = 20
  #наш рассмотренный выше ПИД-руегулятор
- qn0 = L * (1 + (HP / T9)) 
- qn1 = -L * (1 + 2 * (HP / T9) - (T9 / H))
- qn2 = L * (HP / T9)
- y = [SS, SS]; u = 1.0; u_p = 1.0; e = [Q - SS, Q - SS]; mm = [u,u]
+ qn0 = л * (1 + (хр / т9)) 
+ qn1 = -л * (1 + 2 * (хр / т9) - (т9 / х))
+ qn2 = л * (хр / т9)
+ y = [ус, ус]; u = 1.0; u_p = 1.0; e = [к - ус, к - ус]; им = [u,u]
  #при помощи этого сможем посчитать все нужное
- while abs(y[end] - Q) > 0.1
-  push!(e, Q - y[end])
+ while abs(y[end] - к) > 0.1
+  push!(e, к - y[end])
    u = u_p + qn0 * e[end] + qn1 * e[end - 1] + qn2 * e[end - 2]
-  push!(y, J * y[end] - G * y[end - 1] + V * u + B * sin(u_prev))
+  push!(y, а * y[end] - б * y[end - 1] + в * u + г * sin(u_p))
      u_p = u
-  push!(mm,u)
+  push!(им,u)
  end
  #для вывода нужно это :Х
-println("P"); println("O"); println("U")
- for i in 1:length(p); for i in 1:length(o); for i in 1:length(um)
-  println(y[i]); println(e[i]); println(um[i])
+println("У"); println("Е"); println("И")
+ for i in 1:length(у); for i in 1:length(е); for i in 1:length(им)
+  println(y[i]); println(e[i]); println(им[i])
     end 
    end
   end
