b64f07f35f70d570675d198c2ffbf23d565883d1
Shpak Ilya <113047080+IlyaShpak@users.noreply.github.com>
2022-12-14 20:37:57 +0300

    Update main.py

--- trunk/ii02117/task_03/src/main.py
+++ trunk/ii02117/task_03/src/main.py
@@ -1,6 +1,8 @@
 import sys
-from tabs_with_dialog import *
-class Graph_app(QtWidgets.QMainWindow):
+from PyQt5.QtWidgets import QMainWindow as main_window
+import PyQt5.QtWidgets as Qt
+from tabs_with_dialog import Ui_MainWindow
+class Graph_app(main_window):
     def __init__(self):
         super(Graph_app,self).__init__()
         # Create the maptlotlib FigureCanvas object,
@@ -8,13 +10,14 @@ class Graph_app(QtWidgets.QMainWindow):
         self.ui=Ui_MainWindow()
         self.ui.setupUi(self)
         self.init_UI()
+        #self.ui.pushButton.clicked.connect(self.open_other_window)
         self.show()
     def init_UI(self):
         self.setWindowTitle("Редактор графа")
 def main():
-    app = QtWidgets.QApplication(sys.argv)
+    app = Qt.QApplication(sys.argv)
     application = Graph_app()
     application.show()
     sys.exit(app.exec())
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
