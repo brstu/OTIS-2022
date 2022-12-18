36af055738e7c1c8a96b9172aab8107eb301ff85
Nika Levanevskaya <chipolinchikaloe@gmail.com>
2022-12-18 18:22:05 +0300

    mew

--- trunk/ii02212/task_02/src/lab2.jl
+++ trunk/ii02212/task_02/src/lab2.jl
@@ -5,7 +5,7 @@ using Plots
 value_u_t = 0.0 # control action
 _this_is_a_value_time_ = 15 # time
 constant_TO = 0.1 # step
-constant_w_t = 10 # system functioning algorithm
+this_is_a_constant_w_t = 10 # system functioning algorithm
 this_is_a_constant_whanoke_a = 0.2  # constant
 constant_K = 5 # transmission ratio
 this_is_a_constant_karekau_b = 0.3  # constant
@@ -21,16 +21,16 @@ function my_only_function()
     _y_t_ = 1.5 # input temperature
     _y_t_early = 0 # previous input temperature
     println("linear model")
-    i = 1
+    i = 1 # iterations
     while i <= _this_is_a_value_time_
         global value_u_t
-        err = constant_w_t - _y_t_ # error
-        e_early = err # previous error
-        e_2early = e_early # twice previous error
+        mistake = this_is_a_constant_w_t - _y_t_ # error
+        mistake_early = mistake # previous error
+        mistake_two_early = mistake_early # twice previous error
         _q_00_ = constant_K * (1 + (constant_td / constant_TO)) # controller parameters
         _q_01_ = -constant_K * (1 + 2*(constant_td / constant_TO) - (constant_To / constant_T)) # controller parameters
         _q_02_ = constant_K * (constant_td / constant_TO) # controller parameters
-        D_value_u_t = _q_00_ * err + _q_01_ * e_early + _q_02_ * e_2early # delta control action
+        D_value_u_t = _q_00_ * mistake + _q_01_ * mistake_early + _q_02_ * mistake_two_early # delta control action
         u_early = value_u_t # previous control action
         value_u_t = u_early + D_value_u_t
         temperature = _y_t_early
@@ -41,6 +41,7 @@ function my_only_function()
         push!(array_yt_linear, _y_t_)
     end
 end
-
+# function launch
 my_only_function()
+# a graph is being built
 plot(1:15, array_yt_linear, title = "Temperature dependence with PID controller", label = "linear dependence",  lw = 3)
